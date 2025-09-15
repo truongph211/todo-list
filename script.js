ad3c5ea0de45c8856eadea57a79e01e51bdbfdfb
PhuongTruong <73217004+truongph211@users.noreply.github.com>
2025-09-15 23:04:59 +0700

    the first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function updateCounters() {
+  const completedTasks = document.querySelectorAll(".completed").length;
+  const uncompletedTasks = document.querySelectorAll("li:not(.completed)").length;
+
+  completedCounter.textContent = completedTasks;
+  uncompletedCounter.textContent = uncompletedTasks;
+}
+
+function addTask() {
+  const task = inputBox.value.trim();
+  if (!task) {
+    alert("Please enter a task.");
+    return;
+  }
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <label>
+      <input type="checkbox">
+      <span>${task}</span>
+    </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
+  listContainer.appendChild(li);    
+  inputBox.value = "";
+
+  const checkbox = li.querySelector("input");
+  const editBtn = li.querySelector(".edit-btn");
+  const deleteBtn = li.querySelector(".delete-btn");
+  const taskSpan = li.querySelector("span");
+
+  checkbox.addEventListener("click", function() {
+    li.classList.toggle("completed", checkbox.checked);
+    updateCounters();
+  });
+  editBtn.addEventListener("click", function() {
+    const update = prompt("Edit task:", taskSpan.textContent);
+    if (update !== null) {
+      taskSpan.textContent = update;
+      li.classList.remove("completed");
+        checkbox.checked = false;
+        updateCounters();   
+    }
+  });
+  deleteBtn.addEventListener("click", function() {
+    if (confirm("Are you sure you want to delete this task?")) {
+      li.remove();
+      updateCounters();
+    }
+  });
+  updateCounters();
+}
+
+inputBox.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
